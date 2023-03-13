4e926326f866655765d866c8262cb8618bad78ee
SKurbatova <58175476+SofiFairyTell@users.noreply.github.com>
2023-03-13 12:14:54 +0300

    first comm

---
+++ src/ru/bstu/itz/kats/Main.java
@@ -0,0 +1,17 @@
+package ru.bstu.itz.kats;
+
+public class Main {
+    /**<p>Описание команд управления</p>
+     * Для управленияп программной предусмотрены такие команды:
+     *<ul>
+     * <li>first    -   для запуска функции </li>
+     * <li>second   -   для запуска функции </li>
+     * <li>tree     -   для запуска функции </li>
+     * <li>four     -   для запуска функции </li>
+     * <li>end      -   для завершения работы с программой</li>
+     * </ul> */
+
+    public static void main(String[] args) {
+        System.out.print("Введите команду: ");
+    }
+}
