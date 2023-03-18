e44778460eab62e109493943d3f37396096c2230
SKurbatova <58175476+SofiFairyTell@users.noreply.github.com>
2023-03-18 22:27:36 +0300

    upd

--- src/ru/bstu/itz/kats/Main.java
+++ src/ru/bstu/itz/kats/Main.java
@@ -1,17 +1,53 @@
 package ru.bstu.itz.kats;
 
+import LabFunctions.lab1.*;
+
+import java.util.Scanner;
+
 public class Main {
-    /**<p>Описание команд управления</p>
-     * Для управленияп программной предусмотрены такие команды:
-     *<ul>
-     * <li>first    -   для запуска функции </li>
-     * <li>second   -   для запуска функции </li>
-     * <li>tree     -   для запуска функции </li>
-     * <li>four     -   для запуска функции </li>
-     * <li>end      -   для завершения работы с программой</li>
-     * </ul> */
 
     public static void main(String[] args) {
-        System.out.print("Введите команду: ");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Выберите действие:");
+            System.out.println("1. Проверить числа");
+            System.out.println("2. Выход");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Введите первое число: ");
+                    int a = scanner.nextInt();
+
+                    System.out.print("Введите второе число: ");
+                    int b = scanner.nextInt();
+
+                    System.out.print("Введите третье число: ");
+                    int c = scanner.nextInt();
+
+                    checkNumbers(a, b, c);
+
+                    break;
+
+                case 2:
+                    System.out.println("Выход.");
+                    return;
+
+                default:
+                    System.out.println("Неверный выбор.");
+            }
+        }
+    }
+
+    private static void checkNumbers(int a, int b, int c) {
+        ThreeNumbers tn = new ThreeNumbers(a, b, c);
+
+        if (tn.hasPositiveSum()) {
+            System.out.println("Сумма каких-либо двух чисел является положительной.");
+        } else {
+            System.out.println("Сумма каких-либо двух чисел не является положительной.");
+        }
     }
 }
